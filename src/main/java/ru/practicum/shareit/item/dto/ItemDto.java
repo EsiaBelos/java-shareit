b3f87ea5264cf1562c69317e69aea540371de9dd
Kseniya <ksenia-belos@mail.ru>
2023-07-12 23:33:09 +0300

    feature: User Crud

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * TODO Sprint add-controllers.
  */
+@AllArgsConstructor
+@Getter
 public class ItemDto {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean isAvailable;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,30 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Validated
+@Builder
 public class Item {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    private Boolean isAvailable;
+
+    private User owner;
+
+    private ItemRequest request;
 }
